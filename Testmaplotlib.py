ae28206ac38facae8bfd179ec1ada9026a2fecac
qhjstc <654390596@qq.com>
2021-03-10 17:25:00 +0800

    informal submit

--- Testmaplotlib.py
+++ Testmaplotlib.py
@@ -8,8 +8,15 @@ import numpy as np
 
 def main():
     x = np.linspace(-3, 3, 50)
-    
+    y1 = 2 * x + 1
+    y2 = x**2
+    plt.figure()                  # creat the first figure
+    plt.plot(x, y1)
 
+    plt.figure(figsize=(8, 5))                  # creat the second figure
+    plt.plot(x, y2, color="red", linewidth=1.0, linestyle='--')
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
