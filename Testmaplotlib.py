807cae0acb5f8603af6e433a494583d90489fe03
qhjstc <654390596@qq.com>
2021-03-10 21:26:29 +0800

    simple modulation

--- Testmaplotlib.py
+++ Testmaplotlib.py
@@ -1,5 +1,6 @@
 """
 Test matplotlib
+come from mofan python
 """
 
 import matplotlib.pyplot as plt
@@ -14,8 +15,8 @@ def main():
     plt.plot(x, y1)
 
     plt.figure(figsize=(8, 5))                  # creat the second figure
-    plt.plot(x, y1, color="red", linewidth=1.0, linestyle='--')
-    plt.plot(x, y2)
+    l1, = plt.plot(x, y1, color="red", linewidth=1.0, linestyle='--', label='down')
+    l2, = plt.plot(x, y2, label='up')
 
     plt.xlim((-1, 2))
     plt.ylim((-2, 3))
@@ -29,6 +30,14 @@ def main():
     plt.yticks([-2, -1.8, -1, 1.22, 3],
                [r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ \alpha good\ \alpha$'])
     # r regular expression, $: Beautify font, '\': Escape character
+    # gca: get current axis
+    ax = plt.gca()
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_position(('data', 0))
+    ax.spines['left'].set_position(('data', 0))
+
+    plt.legend(handles=[l1, l2], labels=['aaa', 'bbb'], loc='best')
     plt.show()
 
 
